73dd47f4d8a0cd108a4005e74f577b9eddb510a7
ramanji4 <ramanji181818@gmail.com>
2025-07-19 18:31:54 +0530

    terraform-infra-dev

--- 99-cdn/data.tf
+++ 99-cdn/data.tf
@@ -6,6 +6,6 @@ data "aws_cloudfront_cache_policy" "cacheOptmised" {
   name = "Managed-CachingOptimized"
 }
 
-data "aws_ssm_parameter" "https_certificate_arn" {
-  name = "/${var.project_name}/${var.environment}/https_certificate_arn"
+data "aws_ssm_parameter" "https_acm_certificate_arn" {
+  name = "/${var.project_name}/${var.environment}/https_acm_certificate_arn"
 }
